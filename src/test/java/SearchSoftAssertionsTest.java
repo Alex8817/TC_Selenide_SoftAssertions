a14931fd8d32ec5e260a9f814d6bc5af9d3b51cc
Alex8817 <alex.ivanov0988@gmail.com>
2023-10-01 18:15:08 +0200

    add gradle and test

---
+++ src/test/java/SearchSoftAssertionsTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchSoftAssertionsTest {
+
+    @Test
+    void
+    findOnTheSoftAssertionsPageSampleCodeForJUnit5() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("div#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+    }
+}
+
